538dfce9cb3236460125055f4080a1b0a0a800e9
SaintHazard <96043345+SaintHazard@users.noreply.github.com>
2022-10-27 00:33:22 +0900

    [UPDATE] 2.0 행성앱 만들기

---
+++ lib/planets/ui/home/HomePage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_planets_app/planets/ui/home/GradientAppBar.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: const [
+          GradientAppBar("treva"),
+        ],
+      ),
+    );
+  }
+}
